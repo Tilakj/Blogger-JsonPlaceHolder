f87c9a8247244cd07c8e6642868c177db111c08f
tilak.jayanna <tilak.jayanna@epicor.com>
2019-12-28 02:28:41 +0530

    Initial Commit: ReactJs App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,123 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import clsx from 'clsx';
+import { useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import PersonIcon from '@material-ui/icons/Person';
+import MailIcon from '@material-ui/icons/Mail';
+import { BrowserRouter, Link, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home'
+import Users from './components/Users'
+import Posts from './components/Posts'
+import UserShowPage from './components/UserShowPage'
+import PostShowPage from './components/PostShowPage'
+import { useStyles } from './styles'
+import { Redirect } from 'react-router-dom';
+
+
+export default function App() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+    return (
+        <div className={classes.root}>
+            <BrowserRouter>
+                <CssBaseline />
+                <AppBar
+                    position="fixed"
+                    className={clsx(classes.appBar, {
+                        [classes.appBarShift]: open,
+                    })}>
+
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={handleDrawerOpen}
+                            edge="start"
+                            className={clsx(classes.menuButton, {
+                                [classes.hide]: open,
+                            })}>
+
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" noWrap>
+                            Blogger
+                     </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    variant="permanent"
+                    className={clsx(classes.drawer, {
+                        [classes.drawerOpen]: open,
+                        [classes.drawerClose]: !open,
+                    })}
+                    classes={{
+                        paper: clsx({
+                            [classes.drawerOpen]: open,
+                            [classes.drawerClose]: !open,
+                        }),
+                    }}>
+                    <div className={classes.toolbar}>
+                        <IconButton onClick={handleDrawerClose}>
+                            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                        </IconButton>
+                    </div>
+                    <Divider />
+                    <List>
+                        <ListItem button component={Link} to={'/home'}>
+                            <ListItemIcon><DashboardIcon /></ListItemIcon>
+                            <ListItemText primary="Dashboard" />
+                        </ListItem>
+                        <ListItem button component={Link} to={'/users'}>
+                            <ListItemIcon><PersonIcon /></ListItemIcon>
+                            <ListItemText primary="Users" />
+                        </ListItem>
+
+                        <ListItem button component={Link} to={'/posts'}>
+                            <ListItemIcon><MailIcon /></ListItemIcon>
+                            <ListItemText primary="Posts" />
+                        </ListItem>
+                    </List>
+                    <Divider />
+                </Drawer>
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+
+                    <Route path="/home" component={Home} />
+                    <Route path="/users" component={Users} exact={true} />
+                    <Route path="/posts" component={Posts} exact={true} />
+                    <Route path="/users:id" component={UserShowPage} />
+                    <Route path="/posts:id" component={PostShowPage} />
+                  
+                    
+                    <Route render={() => <Redirect to={{ pathname: "/home" }} />} />
+
+                </main>
+            </BrowserRouter>
+        </div >
+
+    );
 }
-
-export default App;
